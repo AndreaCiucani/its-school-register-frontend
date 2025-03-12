e72aade1f52aec57bbe90c89835f52070099c49d
Andrea <andrea.ciucani@itssmartacademy.it>
2025-03-12 17:55:43 +0100

    modified 12-3-25

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ var bodyParser = require('body-parser')
 const conn = require('./connector')
 const port = 3000
 const jwt = require('jsonwebtoken')
+const { request } = require('http')
 
 // generate jwt secret key (one time - copy to env variables)
 // let secretkey = require('crypto').randomBytes(64).toString('hex');
@@ -27,7 +28,7 @@ app.get('/getuser', authenticateToken, async (req, res) => {
     let id_user = req.query.id;
     console.log(id_user);
     try {
-        const [data] = await conn.execute(`select * from users where id = ? LIMIT 1`, [id_user]);
+        const [data] = await conn.execute(`select * from users where id = ? LIMIT 1`, [req.user.userid]);
         data.forEach((row) => {
             console.log(`${row.id} = ${row.lastname} ${row.firstname}`);
         });
@@ -87,16 +88,19 @@ app.post('/createuser', jsonParser, authenticateToken, async (req, res) => {
 app.post('/login', jsonParser, async (req, res) => {
     let requestbody = req.body;
     try {
-        let hash = crypto.createHash('sha256').update(requestbody.pwd).digest('hex');
-        const data = await conn.query(`select id from users where email = ? and password = ? and active = 1`, [requestbody.email, hash]);
-        if (data[0].length < 1) {
+        var hash = crypto.createHash('sha256').update(requestbody.pwd).digest('hex');
+        const [data] = await conn.query(`select u.id, urc.id_role, urc.id_course from users u
+              inner join users_roles_courses urc on urc.id_user = u.id
+              where u.email = ? and u.password = ? and u.active = 1 `, [requestbody.email, hash]);
+        if (data.length < 1) {
             res.json({ error: true, errormessage: "INVALID_USERPWD" });
         } else {
-            const token = generateAccessToken({ username: requestbody.email });
+            const payload = { username: requestbody.email, userid: data[0]["id"], roles: data };
+            const token = generateAccessToken(payload);
             res.json({ error: false, errormessage: "", token: token });
         }
     } catch (err) {
-        console.log(err);
+        console.log("Login Error: " + err);
         res.json({ error: true, errormessage: "GENERIC_ERROR" });
     }
 })
@@ -137,7 +141,7 @@ app.patch('/updateuser/:id', jsonParser, authenticateToken, async (req, res) => 
         }
 
         //update user
-        const data = await conn.execute(`update users set lastname = ?, firstname = ?, phone = ?, email = ?, active = ?, fiscalcode = ? where id = ?`, [requestbody.lastname, requestbody.firstname, requestbody.phone, requestbody.email, requestbody.active, requestbody.fiscalcode, patchid]);
+        const data = await conn.execute(`update users set lastname = ?, firstname = ?, phone = ?, active = ?, fiscalcode = ? where id = ?`, [requestbody.lastname, requestbody.firstname, requestbody.phone, requestbody.active, requestbody.fiscalcode, patchid]);
         res.json(data);
 
     } catch (err) {
@@ -168,21 +172,155 @@ app.post('/updatepwd', jsonParser, authenticateToken, async (req, res) => {
     }
 })
 
-
-app.post('/Register', jsonParser, (req, res) => {
-    console.log(req.body);
-    res.statusCode = 200;
-    res.send('Register')
-})
-
-
 app.listen(port, () => {
     console.log(`Its Register app listening on port ${port}`)
 })
 
 
-function generateAccessToken(username) {
-    return jwt.sign(username, process.env.TOKEN_SECRET, { expiresIn: '1800s' });
+app.post('/createcourse', jsonParser, authenticateToken, async (req, res) => {
+    let requestbody = req.body;
+    try {
+
+        const validation = await conn.query(`select name from courses where name = ? and year = ?`, [requestbody.name, requestbody.year]);
+        if (validation[0].length < 1) {
+
+            const [data] = await conn.execute(`insert into courses (name, year, period) values (?,?,?)`, [requestbody.name, requestbody.year, requestbody.period]);
+            res.json(data);
+        } else {
+            res.json({ error: true, errormessage: "COURSE_EXISTS" });
+        }
+
+    } catch (err) {
+        console.log(err);
+        res.json({ error: true, errormessage: "retrieve courses error" });
+    }
+})
+
+app.delete('/deletecourse/:id', authenticateToken, async (req, res) => {
+    let deleteid = req.params.id;
+    try {
+
+        //data validation
+        const validation = await conn.query(`select id from courses where id = ?`, [deleteid]);
+        if (validation[0].length < 1) {
+            res.json({ error: true, errormessage: "INVALID_COURSE_ID" });
+            return;
+        }
+
+        //delete user
+        const data = await conn.execute(`delete from courses where id = ?`, [deleteid]);
+        res.json(data);
+    } catch (err) {
+        console.log("Deletecourse Error: " + err);
+        res.json({ error: true, errormessage: "GENERIC_ERROR" });
+    }
+
+})
+
+app.patch('/updatecourse/:id', jsonParser, authenticateToken, async (req, res) => {
+    let patchid = req.params.id;
+    let requestbody = req.body;
+    try {
+
+        //data validation
+        const validation = await conn.query(`select id from courses where id = ?`, [patchid]);
+        if (validation[0].length < 1) {
+            res.json({ error: true, errormessage: "INVALID_COURSE_ID" });
+            return;
+        }
+
+        //update course
+        const data = await conn.execute(`update courses set name = ?, year = ?, period = ? where id = ?`, [requestbody.name, requestbody.year, requestbody.period, patchid]);
+        res.json(data);
+
+    } catch (err) {
+        console.log("Updatecourse Error: " + err);
+        res.json({ error: true, errormessage: "GENERIC_ERROR" });
+    }
+})
+
+app.get('/getallcourses', authenticateToken, async (req, res) => {
+
+    try {
+        const [data] = await conn.query(`select * from courses`);
+        res.json(data);
+
+    } catch (err) {
+        console.log(err);
+        res.json({ error: true, errormessage: "retrieve courses error" });
+    }
+})
+
+app.get('/getusercourses', authenticateToken, async (req, res) => {
+
+    try {
+        const [data] = await conn.query(`select distinct c.* from users_roles_courses urc inner join courses c on urc.id_course = c.id where urc.id_user = ?`, [req.user.userid]);
+        res.json(data);
+    } catch (err) {
+        console.log(err);
+        res.json({ error: true, errormessage: "retrieve usercourses error" });
+    }
+})
+
+app.post('/linkcourse', authenticateToken, async (req, res) => {
+    let requestbody = req.body;
+    try {
+        //data validation
+        let validation = await conn.query(`select * from users_roles_courses where id_user = ? and id_role = ? and id_course = ?`, [requestbody.user, requestbody.role, requestbody.id_course]);
+        if (validation[0].length < 1) {
+            res.json({ error: true, errormessage: "LINK_ALREADY_EXISTS" });
+            return;
+        }
+        validation = await conn.query(`select id from users where id_user = ?`, [requestbody.id_user]);
+        if (validation[0].length < 1) {
+            res.json({ error: true, errormessage: "USER_NOT_EXISTS" });
+            return;
+        }
+        validation = await conn.query(`select id from roles where id_role = ?`, [requestbody.id_role]);
+        if (validation[0].length < 1) {
+            res.json({ error: true, errormessage: "ROLE_NOT_EXISTS" });
+            return;
+        }
+        validation = await conn.query(`select id from courses where id_course = ?`, [requestbody.id_course]);
+        if (validation[0].length < 1) {
+            res.json({ error: true, errormessage: "COURSE_NOT_EXISTS" });
+            return;
+        }
+
+    //add role/course
+    const data = await con.execute(`INSERT INTO users_roles_courses (id_user, id_role, id_course) VALUES (?,?,?)`, [requestbody.id_user, requestbody.id_role, requestbody.id_course]);
+    res.json(data);
+  } catch (err) {
+    console.log("linkcourse Error:" + err);
+    res.json({ error: true, errormessage: "GENERIC_ERROR" });
+  }
+})
+
+app.post('/unlinkcourse', jsonParser, authenticateToken, async (req, res) => {
+    let requestbody = req.body;
+    try {
+        //data validation
+        let validation = await con.query(`select * from users_roles_courses where id_user = ? and id_role = ? and id_course = ?`, [requestbody.id_user, requestbody.id_role, requestbody.id_course]);
+        if (validation[0].length < 1) {
+            res.json({ error: true, errormessage: "LINK_NOT_EXISTS" });
+            return;
+        }
+        //add role/course
+        const data = await con.execute(`DELETE FROM users_roles_courses WHERE id_user = ? AND id_role = ? AND id_course = ?`, [requestbody.id_user, requestbody.id_role, requestbody.id_course]);
+        res.json(data);
+    } catch (err) {
+        console.log("unlinkcourse Error:" + err);
+        res.json({ error: true, errormessage: "GENERIC_ERROR" });
+    }
+})
+
+
+
+
+
+
+function generateAccessToken(payload) {
+    return jwt.sign(payload, process.env.TOKEN_SECRET, { expiresIn: '5800s' });
 }
 
 function authenticateToken(req, res, next) {
